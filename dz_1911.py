82d7fffc01d1a242317314cc77ba3b1785686c98
Ulianis <savinauliana666@gmail.com>
2024-12-02 17:15:01 +0500

    initial commit

---
+++ dz_1911.py
@@ -0,0 +1,87 @@
+# task1
+
+# tuple_1 = ('Роман', 'Новелла', 'Фэнтези', 'Научная Фантастика')
+# tuple_2 = (3, 7, 9, 1, 6, 8, 2, 5, 4)
+
+# tuple_1_len = len(tuple_1)
+# tuple_2_len = len(tuple_2)
+# print(tuple_1_len)
+# print(tuple_2_len)
+
+# tuple_1_min = min(tuple_1)
+# tuple_1_max = max(tuple_1)
+# tuple_2_min = min(tuple_2)
+# tuple_2_max = max(tuple_2)
+# print(tuple_1_max)
+# print(tuple_1_min)
+# print(tuple_2_max)
+# print(tuple_2_min)
+
+# tuple_2_sum = sum(tuple_2)
+# print(tuple_2_sum)
+
+# sortedmax_tuple_1 = sorted(tuple_1)
+# sortedmax_tuple_2 = sorted(tuple_2)
+# new_tuple_1max = tuple(sortedmax_tuple_1)
+# new_tuple_2max = tuple(sortedmax_tuple_2)
+
+# sortedmin_tuple_1 = sorted(tuple_1, reverse=True)
+# sortedmin_tuple_2 = sorted(tuple_2, reverse=True)
+
+
+# print(new_tuple_1max)
+# print(new_tuple_2max)
+
+# task2
+
+# cinema_genres = ("Комедия", "Экшн", "Пеплум", "Триллер")
+# new_cinema_genres = ("Комедия", "Боевик", "Пеплум", "Триллер")
+# print(new_cinema_genres)
+
+# n_cinema_genres = ("Комедия", "Боевик", "Пеплум", "Триллер", "Фэнтези")
+# print(n_cinema_genres)
+
+# def convert_cinema(new_cinema_genres):
+#     str=''.join(new_cinema_genres)
+#     return str
+# str_cinema = convert_cinema(new_cinema_genres)
+# print(f'Обновленные жанры кино: {str_cinema}')
+
+
+
+# пускай тут будут вещи для похода)
+
+# task3
+
+# me = {'палатка', 'зажигалка', 'нож', 'аптечка', 'фонарик'} 
+# friend = {'аптечка', 'немного кухонной утвари', 'спальник', 'еда/вода'}
+
+# new_set = me.union(friend)
+# print(new_set)
+
+# print(me)
+# print(friend)
+
+# new_set1 = me.intersection(friend)
+# print(new_set1)
+
+
+
+
+# task4
+
+# cinema_genres = ["комедия", "экшн", "пеплум", "триллер", "комедия"]
+# cinema_genres = set(cinema_genres)
+
+# cinema_genres.add('боевик')
+# cinema_genres.add('фэнтези')
+# print(cinema_genres)
+
+# cinema_genres.remove('боевик')
+# print(cinema_genres)
+
+# cinema_genres.pop()
+# print(cinema_genres)
+
+# cinema_genres = list(cinema_genres)
+
